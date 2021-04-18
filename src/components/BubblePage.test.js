7addf24ef3d6b89e10fca136d42aad693c923c42
Elijah <elijahoneal@yahoo.com>
2021-04-17 23:40:47 -0400

    Interview Questions

--- src/components/BubblePage.test.js
+++ src/components/BubblePage.test.js
@@ -1,13 +1,33 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
+import { fetchBubbles as mockFetchBubbles} from '../api/fetchBubbles'
 import BubblePage from "./BubblePage";
+
+jest.mock('../api/fetchBubbles')
+
+const sampleBubble = {
+  code: {hex: "#f0f8ff"},
+  color: 'aliceblue2',
+  id:11
+}
 
 test("Renders BubblePage without errors", () => {
   // Finish this test
+  render(<BubblePage/>)
 });
 
-test("Fetches data and renders the bubbles on mounting", () => {
+test("Fetches data and renders the bubbles on mounting", async () => {
   // Finish this test
+  render(<BubblePage/>)
+  mockFetchBubbles.mockResolvedValuesOnce({
+    data: sampleBubble
+  })
+
+  await waitFor( () => {
+    const bubbleContainer = screen.getByTestId("container")
+    expect(bubbleContainer).toBeDefined();
+  } )
+
 });
 
 //Task List
